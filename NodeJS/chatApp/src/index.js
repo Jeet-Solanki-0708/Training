2067a03ebb079e2c69c0e0c77bb53208a1eaf3d6
shahdhairya6789 <shahdhairya678@gmail.com>
2020-08-24 12:04:53 +0530

    Chat App

---
+++ NodeJS/chatApp/src/index.js
@@ -0,0 +1,45 @@
+const path = require('path')
+const http = require('http')
+const express = require('express')
+const socketio = require('socket.io')
+const Filter = require('bad-words')
+const { generateMessage, generateLocationMessage } = require('./utils/messages')
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+const publicPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicPath))
+
+io.on('connection', (socket) => {
+    console.log('New WebSocket connection')
+
+    socket.emit('message', generateMessage('Welcome!'))
+    socket.broadcast.emit('message', generateMessage('A new user has joined!'))
+
+    socket.on('sendMessage', (message, callback) => {
+        const filter = new Filter()
+
+        if (filter.isProfane(message)) {
+            return callback('Profanity is not allowed!')
+        }
+
+        io.emit('message', generateMessage(message))
+        callback()
+    })
+
+    socket.on('sendLocation', (coords, callback) => {
+        io.emit('locationMessage', generateLocationMessage(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
+        callback()
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', generateMessage('A user has left!'))
+    })
+})
+
+server.listen(3000, () => {
+    console.log('Server running')
+})
